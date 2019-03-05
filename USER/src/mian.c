5096f323a567e383c58755568343b1ebd5cbe1bd
meetwit <2424972097@qq.com>
2019-03-05 11:21:49 +0800

    增加上电校准

--- USER/src/mian.c
+++ USER/src/mian.c
@@ -7,17 +7,6 @@
 #include "usart3.h"
 #include "stdio.h" 
 
-/*
-陀螺仪配置
-波特率 		115200
-安装方式		垂直
-回传速率		10HZ
-超时时间设置为20ms < 100ms（10HZ）
-
-连接串口2配置陀螺仪的时候 注意 将串口引脚设置为浮空输入
-			usart2.c 第35行
-*/
-
 #define  ADDO PAin(4)
 #define  ADSK PAout(5)
 
@@ -49,13 +38,29 @@ u32 ReadCount(void)
  return(Count);
 }
 
+float msg(u8 time){
+	u8 time_temp;
+	u32 mesg_total = 0;
+	float ch_N = 0;
+	
+	time_temp = time;
+	
+	for(;time>0;time--)
+		{
+			mesg_total+=ReadCount();
+		}
+			
+	//ch_N=c*50*9.8/8388608.0*2.55;
+	
+	ch_N=mesg_total/time_temp*50/8388608.0*2.55;
+
+	return ch_N;
+
+}
 
 int main(){
 	
-	u32 a,b,c;
-	
-	float ch0_N,ch1_N,ch_N;
-	float ch0_V,ch1_V;
+	float biancha = 0;
 	
 Stm32_Clock_Init(9);	//系统时钟设置	
 	
@@ -65,43 +70,21 @@ Stm32_Clock_Init(9);	//系统时钟设置
 	/*串口1初始化，波特率为9600*/
 	usart_init(115200);			//串口1作为监控
 	
-	/*串口2初始化，波特率为9600*/
-	//usart2_init(115200);			//串口2作为陀螺仪
-	
-	/*串口3初始化，波特率为9600*/
-	//usart3_init(19200);			//串口3无线连接
 
 	delay_init(72);					//延时函数初始化
-	Adc_Init();							//PA1,开启ADC通道一
-	
-	printf("start\r\n");
+
+	biancha = msg(10);
+	printf("start biancha=%f\r\n",biancha);
 	
 	while(1){
 		
-		//Task_Pc();
-		//Task_Pc2();
-		//Task_Pc3();
-		
-		if(send_flag>=2){
-			a=Get_Adc(6);	//ch0
-			b=Get_Adc(7);	//ch1
-			c=ReadCount();
+		if(send_flag>=5||1){
 			
-			ch_N=c*50*9.8/8388608.0*2.55;
-			
-			ch0_N=a*30*9.8/4096.0;
-			ch1_N=b*30*9.8/4096.0;
-			
-			ch0_V=a*3.3/4096.0;
-			ch1_V=b*3.3/4096.0;
-			
-//			printf("ch0=,%d,,ch1=,%d,\r\n",a,b);
-//			printf("ch0_V=,%f,V,ch1_V=,%fV,\r\n",ch0_V,ch1_V);
-//		printf("ch0_N=,%f,N,ch1_N=,%fN\r\n",ch0_N,ch1_N);
-//			printf("%f,%f",ch0_N,ch1_N);
-//		printf("c=%d,c=%fN,\r\n",c,ch_N);
-				printf("%f\r\n",ch_N);
-				send_flag=0;
+//			printf("send_flag = %d\r\n",send_flag);
+			printf("%.3f\r\n",msg(5)-biancha);
+//			printf("send_flag = %d\r\n",send_flag);
+
+			send_flag=0;
 		}
 	}
 
